441d11b7ea3dbef3f13e4f3bc61e92e92ef8cd0d
Timothy J. Moriarty <X00281296@wcc.dom>
2019-02-26 11:30:53 -0600

    adding initial interest calculator

---
+++ interest.py
@@ -0,0 +1,22 @@
+def getInput():
+    interest_rate = float(input("Enter Interest Rate: "))
+    dollar_amt = float(input("Enter Dollar Amount: "))
+
+    while interest_rate < 0:
+        interest_rate = float(input("Error please enter positive number.  Enter Interest Rate: "))
+
+    while dollar_amt < 0:
+        dollar_amt = float(input("Error please enter positive number.  Enter Dollar Amount: "))
+
+    return interest_rate, dollar_amt
+
+def calc_interest(interest_rate, dollar_amt):
+    new_amt = dollar_amt * (1 + interest_rate)
+    return new_amt
+
+def outputResult(dollar_amt):
+    print(dollar_amt)
+
+interest_rate, dollar_amt = getInput()
+new_amt = calc_interest(interest_rate, dollar_amt)
+outputResult(new_amt)
